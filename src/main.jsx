45dbbae930d54a5be7cd5c7465de51e25276753d
Pohrebniak Andrian <pogra29@gmail.com>
2024-02-12 00:18:46 +0200

    Add persist storage

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+
 import { App } from "./App.jsx";
+
 import "modern-normalize";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,18 @@
 import { devToolsEnhancer } from "@redux-devtools/extension";
 import { createStore } from "redux";
 import { rootReducer } from "./reducer";
-
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
 const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, enhancer);
+export const persistor = persistStore(store);
